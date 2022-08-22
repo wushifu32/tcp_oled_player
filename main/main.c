e553f9e0f1e56c75688706b92310b631b1b29be6
Wu Bo <wubo.oduw@gmail.com>
2022-08-22 08:51:08 +0800

    add tcp server

--- main/main.c
+++ main/main.c
@@ -8,6 +8,7 @@
 #include "freertos/task.h"
 #include "esp_freertos_hooks.h"
 #include "freertos/semphr.h"
+#include "freertos/stream_buffer.h"
 #include "esp_system.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
@@ -31,6 +32,8 @@
 #define KEEPALIVE_IDLE              (5)
 #define KEEPALIVE_INTERVAL          (5)
 #define KEEPALIVE_COUNT             (3)
+#define FRAME_SIZE                  (128*64/8)
+#define STREAM_BUF_SIZE             (4*FRAME_SIZE)
 
 const LV_ATTRIBUTE_MEM_ALIGN uint8_t mbin_map[] = {
 0xFF,0xFF,0xFF,0xFF,0xFF,0xC0,0x3F,0xFF,0xFF,0xFB,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,\
@@ -99,19 +102,124 @@ const LV_ATTRIBUTE_MEM_ALIGN uint8_t mbin_map[] = {
 0xFF,0xFF,0xFF,0xFF,0xFF,0x00,0x00,0x00,0x03,0x80,0x00,0x01,0xFF,0xFF,0xFF,0xFF,\
 };
 
-const lv_img_dsc_t mbin = {
+lv_img_dsc_t mbin = {
   .header.cf = LV_IMG_CF_ALPHA_1BIT,
   .header.always_zero = 0,
   .header.reserved = 0,
   .header.w = 128,
   .header.h = 64,
-  .data_size = 1024,
+  .data_size = FRAME_SIZE,
   .data = mbin_map,
 };
 
 static void lv_tick_task(void *arg);
 static void guiTask(void *pvParameter);
 static void create_demo_application(void);
+static uint8_t frame_buffer[FRAME_SIZE];
+static int frame_cnt;
+StreamBufferHandle_t stream_buf;
+lv_obj_t *frame;
+static int play_start;
+
+static void do_retransmit(const int sock)
+{
+    int len;
+    char rx_buffer[2048];
+
+    do {
+        len = recv(sock, rx_buffer, sizeof(rx_buffer), 0);
+        if (len < 0) {
+            ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
+        } else if (len == 0) {
+	   		play_start = 0;
+            ESP_LOGW(TAG, "Connection closed");
+        } else {
+            int cnt = len;
+            play_start = 1;
+            while (cnt > 0) {
+		    size_t result = xStreamBufferSend(stream_buf, &rx_buffer, cnt, portMAX_DELAY);
+		    cnt -= result;
+	    }
+            //ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
+        }
+    } while (len > 0);
+}
+
+static void tcp_server_task(void *pvParameters)
+{
+    char addr_str[128];
+    int addr_family = (int)pvParameters;
+    int ip_protocol = 0;
+    int keepAlive = 1;
+    int keepIdle = KEEPALIVE_IDLE;
+    int keepInterval = KEEPALIVE_INTERVAL;
+    int keepCount = KEEPALIVE_COUNT;
+    struct sockaddr_storage dest_addr;
+
+    struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
+    dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
+    dest_addr_ip4->sin_family = AF_INET;
+    dest_addr_ip4->sin_port = htons(PORT);
+    ip_protocol = IPPROTO_IP;
+
+    int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+    if (listen_sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        vTaskDelete(NULL);
+        return;
+    }
+    int opt = 1;
+    setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    ESP_LOGI(TAG, "Socket created");
+
+    int err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    if (err != 0) {
+        ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        ESP_LOGE(TAG, "IPPROTO: %d", addr_family);
+        goto CLEAN_UP;
+    }
+    ESP_LOGI(TAG, "Socket bound, port %d", PORT);
+
+    err = listen(listen_sock, 1);
+    if (err != 0) {
+        ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
+        goto CLEAN_UP;
+    }
+
+    while (1) {
+
+        ESP_LOGI(TAG, "Socket listening");
+
+        struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
+        socklen_t addr_len = sizeof(source_addr);
+        int sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+            break;
+        }
+
+        // Set tcp keepalive option
+        setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &keepAlive, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &keepIdle, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &keepInterval, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &keepCount, sizeof(int));
+        // Convert ip address to string
+        if (source_addr.ss_family == PF_INET) {
+            inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
+        }
+
+        ESP_LOGI(TAG, "Socket accepted ip address: %s", addr_str);
+
+        do_retransmit(sock);
+
+        shutdown(sock, 0);
+        close(sock);
+    }
+
+CLEAN_UP:
+    close(listen_sock);
+    vTaskDelete(NULL);
+}
 
 void app_main() {
 
@@ -119,8 +227,9 @@ void app_main() {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
-
-    //xTaskCreate(tcp_server_task, "tcp_server", 4096, (void*)AF_INET, 5, NULL);
+	
+    stream_buf = xStreamBufferCreate(STREAM_BUF_SIZE, FRAME_SIZE);
+    xTaskCreate(tcp_server_task, "tcp_server", 4096*2, (void*)AF_INET, 5, NULL);
 
     /* Pinned task to avoid memory corruption problems */
     xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
@@ -171,10 +280,22 @@ static void guiTask(void *pvParameter) {
     create_demo_application();
 
     while (1) {
-        vTaskDelay(pdMS_TO_TICKS(30));
+        //vTaskDelay(pdMS_TO_TICKS(30));
+        size_t result = xStreamBufferReceive(stream_buf, frame_buffer+frame_cnt ,
+                        FRAME_SIZE-frame_cnt, pdMS_TO_TICKS(40));
+        frame_cnt += result;
+        if (play_start && frame_cnt < FRAME_SIZE) {
+            ESP_LOGW(TAG, "frame lost");
+        }
+        assert(frame_cnt <= FRAME_SIZE);
 
         /* Try to take the semaphore, call lvgl related function on success */
         if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
+            if (frame_cnt == FRAME_SIZE) {
+                 mbin.data = (const uint8_t *)frame_buffer;
+                 frame_cnt = 0;
+                 lv_img_set_src(frame, &mbin);
+            }
             lv_task_handler();
             xSemaphoreGive(xGuiSemaphore);
        }
@@ -188,8 +309,8 @@ static void guiTask(void *pvParameter) {
 static void create_demo_application(void)
 {
     lv_obj_t * scr = lv_disp_get_scr_act(NULL);
-    lv_obj_t *img =  lv_img_create(scr, NULL);
-    lv_img_set_src(img, &mbin);
+    frame =  lv_img_create(scr, NULL);
+    lv_img_set_src(frame, &mbin);
 }
 
 static void lv_tick_task(void *arg) {
